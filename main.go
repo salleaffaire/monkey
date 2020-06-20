c125c7ac1948c5ce6705a6cd78d23227dd27ea85
DESKTOP-2UDOJVU\Luc <salleaffaire@gmail.com>
2020-06-20 10:23:45 -0400

    Ast, Parser for Let ststements

--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"os/user"
+
+	"github.com/salleaffaire/monkey/repl"
 )
 
 func main() {
@@ -13,4 +16,5 @@ func main() {
 	fmt.Printf("Hello %s!\n This is the Monkey programming language!\n",
 		user.Username)
 	fmt.Printf("Feel free to type in commands\n")
+	repl.Start(os.Stdin, os.Stdout)
 }
